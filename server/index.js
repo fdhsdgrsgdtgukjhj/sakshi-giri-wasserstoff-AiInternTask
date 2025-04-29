90e8c68dd668b89b00eca7c4eda0df161661f4d4
fdhsdgrsgdtgukjhj <sakshi2001giri@gmail.com>
2025-04-29 22:46:40 +0530

    cors error fix

--- server/index.js
+++ server/index.js
@@ -35,8 +35,9 @@ app.use(helmet({
 }));
 
 // Configure CORS
+
 app.use(cors({
-  origin: ['http://localhost:5173', process.env.VITE_API_URL || 'http://localhost:3000'],
+  origin: '*' ,
   methods: ['GET', 'POST'],
   allowedHeaders: ['Content-Type', 'X-Persona'],
 }));
